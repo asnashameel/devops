9f75c577fa1b3ece52999c2882f8a673dd88f5db
asnashameel <155845311+asnashameel@users.noreply.github.com>
2025-02-05 16:19:30 +0530

    Delete python directory

--- python/set.py
+++
@@ -1,9 +0,0 @@
-set = set()
-print(type(set))
-for i in range(1,8):
-    set.add(i)
-set.pop()
-for i in set:
-    print(i)
-   
-print(len(set))
--- python/slice.py
+++
@@ -1,5 +0,0 @@
-def array_slicing(strings):
-    str=strings[1:-1]
-    return str
-strings=["hi","hello","word","asna","grrr"]
-print(array_slicing(strings))
