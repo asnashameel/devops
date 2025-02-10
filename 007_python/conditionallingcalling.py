0b39ac98d1959c0c231bd6825e11a7de5df7690e
asnashameel <155845311+asnashameel@users.noreply.github.com>
2025-02-10 16:43:39 +0530

    Add files via upload

---
+++ 007_python/conditionallingcalling.py
@@ -0,0 +1,19 @@
+def square(x):
+    return x**2
+def cube(a):
+    return a**3
+def absolute(b):
+    if b >= 0:
+        return b
+    else:
+        return -(b)
+def Hof(type):
+    if type =='square':
+        return square
+    elif type == 'cube':
+        return cube
+    else:
+        return absolute
+    
+res = Hof('cube')
+print(res(5))
